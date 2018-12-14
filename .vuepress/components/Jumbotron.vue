2ca40e4f9b86400c6bd7b4525c22a9aa174d5177
Yannick Schaus <github@schaus.net>
2018-12-14 14:40:43 +0100

    Upgrade VuePress, improve jumbotron
    Image source: https://play.google.com/store/apps/dev?id=7475306295333689578
    
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- .vuepress/components/Jumbotron.vue
+++ .vuepress/components/Jumbotron.vue
@@ -1,22 +1,18 @@
 <template>
   <div class="jumbotron">
-    <h1 class="welcome slide-title jumbotron-title">
-      <div class="jumbotron-pattern"></div>
-      <span>Welcome to the<img src="/logo-white.png" alt="openHAB" class="openhab-logo" />Foundation!</span>
-    </h1>
-
-    <div style="min-height: 320px">
+    <div>
+      <h1 class="welcome jumbotron-title">
+        <span>Welcome to the<img src="/logo-white.png" alt="openHAB" class="openhab-logo" />Foundation!</span>
+      </h1>
       <ClientOnly>
         <Parallax>
-          <img src="/images/home/library.jpg" class="illustration-fadeIn" />
+          <img src="/images/home/jumbotron.jpg" />
         </Parallax>
       </ClientOnly>
     </div>
     <div class="content jumbotron-center">
 
       <h2>We are a non-profit organisation with the mission to educate the public about the possibilities and benefits of free and open smart home solutions.</h2>
-
-      <!-- <img class="illustration-fadeIn jumbotron-illustration" src="/images/smart-home-2769210_1920.jpg" alt=""> -->
 
       <p>More and more new technologies and devices are introduced to our living, working and business environment. Although all of them are designed to make our lives easier, we believe they have a common problem: The lack of interoperability, due to incompatible technology stacks and conflicting commercial interests. Market consolidation and cooperation between all stakeholders therefore remains a difficult challenge.</p>
 
@@ -27,7 +23,19 @@
 </template>
 
 <style lang="stylus">
+@keyframes slideIn2
+  0%
+    transform translateY(30px)
+    opacity 0
+  40%
+    transform translateY(10px)
+    opacity 0
+  70%
+    transform translateY(0)
+    opacity 1
+
 .jumbotron
+  min-height 480px
   .jumbotron-pattern
     position absolute
     left 0
@@ -38,24 +46,25 @@
     background url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAAECAYAAACp8Z5+AAAAHklEQVQYV2NkQAX/GZH4/4FsRpgAmAOSBBFwDkgAAIKuBARYEptoAAAAAElFTkSuQmCC')
     z-index -100
   .jumbotron-title
-    background #aaa
+    margin-top 0
     font-weight 400
-    margin-top -0rem
-    padding-top 8rem
+    width 100%
     padding-bottom 2rem
-    background linear-gradient(#ff660030, #ff6600ff)
+    margin-top 4rem
+    //background linear-gradient(#ff6600ff, #ff660050)
     color white
-    position relative
+    position absolute
     font-family 'Open Sans', sans-serif
-    font-size 28pt
+    font-size 31pt
     line-height 2
-    //box-shadow 0px 10px 60px 0px rgba(0,0,0,0.75);
-    // span
-    //   border-bottom 4px solid #ff6600
+    text-shadow 0 0 10px #333
+    user-select none
+    animation 1.5s ease-out 0s 1 slideIn2
     .openhab-logo
       padding-left 10px
       padding-right 10px
       margin-bottom -15px
+      filter drop-shadow(0 0 10px #000)
   .jumbotron-center
     text-align center
   .jumbotron-illustration
@@ -66,9 +75,8 @@
     font-weight 300
     border 0
   .Masthead
-      margin-top -1.5rem
-      height: 320px !important
-      min-height: 320px !important
+      height 480px !important
+      min-height 480px !important
 </style>
 
 <script>
